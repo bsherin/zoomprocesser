c661e7295a169117f4cbd1b91ad5c6371ce94b1a
bls910 <bsherin@northwestern.edu>
2023-02-09 13:50:50 -0600

    Updated readme

--- pzdir.py
+++ pzdir.py
@@ -4,8 +4,8 @@ from pzf import do_process
 
 
 def do_dir_process(dir_name):
+    os.chdir(dir_name)
     for fname in os.listdir(dir_name):
-        os.chdir(dir_name)
         if fname[0] == ".":
             continue
         with open(fname) as file:
